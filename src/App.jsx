1f07bb702674f08b08409dd299652454eae60654
Marina2022 <marusiiiia@yandex.ru>
2025-08-07 13:12:36 +0300

    coctails section

--- src/App.jsx
+++ src/App.jsx
@@ -2,20 +2,44 @@ import {ScrollTrigger, SplitText} from 'gsap/all';
 import gsap from 'gsap';
 import Navbar from "./components/Navbar.jsx";
 import Hero from "./components/Hero.jsx";
+import Lenis from "@studio-freight/lenis";
+import {useEffect, useRef} from "react";
+import Cocktails from "./components/Cocktails.jsx";
+
 
 gsap.registerPlugin(ScrollTrigger)
 gsap.registerPlugin(SplitText)
 
 const App = () => {
+  const lenisRef = useRef();
 
+  useEffect(() => {
+    lenisRef.current = new Lenis({
+      duration: 1.2,        // скорость скролла
+      easing: (t) => t,     // функция easing, можно кастомизировать
+      smooth: true,
+      // другие опции - https://studio-freight.github.io/lenis/
+    });
+
+    function raf(time) {
+      lenisRef.current.raf(time);
+      requestAnimationFrame(raf);
+    }
+
+    requestAnimationFrame(raf);
+
+    return () => {
+      // очистка при размонтировании компонента
+      lenisRef.current.destroy();
+    };
+  }, []);
 
   return (
     <main>
       <Navbar/>
       <Hero/>
+      <Cocktails />
 
-      <div className="h-dvh bg-black"></div>
-      <div className="h-dvh bg-black"></div>
 
     </main>
   );
--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -2,76 +2,116 @@ import React, {useRef} from 'react';
 import {useGSAP} from "@gsap/react";
 import gsap from "gsap";
 import {SplitText} from "gsap/SplitText";
+import {useMediaQuery} from "react-responsive";
 
 const Hero = () => {
 
-
+  const videoRef = useRef()
   const mojitoRef = useRef(null)
+
+  const isMobile = useMediaQuery({maxWidth: 676})
 
   useGSAP(() => {
 
-    const splittedMojito = SplitText.create(mojitoRef.current, {type: "chars"})
-    const splittedSubtitle = SplitText.create('.subtitle', {type: "lines"})
+    let splittedMojito, splittedSubtitle
 
-    splittedMojito.chars.forEach(char => {
-      char.classList.add("text-gradient")
-    })
+    document.fonts.ready.then(() => {
 
-    gsap.timeline()
-      .from(splittedMojito.chars, {
-        yPercent: 100,
-        ease: "expo.out",
-        stagger: 0.06,
-        duration: 1.8,
+      requestAnimationFrame(() => {
+        setTimeout(() => {
+
+          splittedMojito = SplitText.create(mojitoRef.current, {type: "chars"})
+          splittedSubtitle = SplitText.create('.subtitle', {type: "lines"})
+
+          splittedMojito.chars.forEach(char => {
+            char.classList.add("text-gradient")
+          })
+
+          gsap.timeline()
+            .from(splittedMojito.chars, {
+              yPercent: 100,
+              ease: "expo.out",
+              stagger: 0.06,
+              duration: 1.8,
+            })
+            .from(splittedSubtitle.lines, {
+              yPercent: 100,
+              ease: "expo.out",
+              stagger: 0.06,
+              duration: 1.8,
+              opacity: 0
+            }, "-=0.8")
+        }, 350) // или 50-100мс для надёжности
       })
-      .from(splittedSubtitle.lines, {
-        yPercent: 100,
-        ease: "expo.out",
-        stagger: 0.06,
-        duration: 1.8,
-        opacity: 0
-      }, "-=0.8")
 
-    gsap.timeline({
-      scrollTrigger: {
-        trigger: '.hero',
-        start: "top top",
-        end: "bottom top",
-        scrub: 1,
-      }
+
+      gsap.timeline({
+        scrollTrigger: {
+          trigger: '#hero',
+          start: "top top",
+          end: "bottom top",
+          scrub: 1,
+        }
+      })
+        .to('.left-leaf', {y: -200}, 0)
+        .to('.right-leaf', {y: 200}, 0)
     })
-      .to('.left-leaf', {y: -200}, 0)
-      .to('.right-leaf', {y: 200}, 0)
+
+
+    videoRef.current.onloadedmetadata = () => {
+      gsap.to(videoRef.current, {
+        scrollTrigger: {
+          trigger: videoRef.current,
+          start: isMobile ? "top 50%" : "center 60%",
+          end: isMobile ? "120% top" : "bottom top",
+          scrub: 1,
+          pin: true,
+          onUpdate: (self) => {
+            videoRef.current.currentTime = self.progress * videoRef.current.duration;
+          }
+        }
+      })
+    }
+
+
+
+
 
   })
 
   return (
-    <section className="noisy" id="hero">
-      <h1 className="title overflow-hidden " ref={mojitoRef}>MOJITO</h1>
-      <img src="/images/hero-left-leaf.png" alt="left-leaf" className="left-leaf"/>
-      <img src="/images/hero-right-leaf.png" alt="right-leaf" className="right-leaf"/>
 
-      <div className="body">
-        <div className="content">
-          <div className="space-y-5 hidden md:block">
-            <p>Cool. Crisp. Classic.</p>
-            <p className="subtitle">
-              Sip the Spirit <br/> of Summer
-            </p>
-          </div>
+    <>
+      <section className="noisy" id="hero">
+        <h1 className="title overflow-hidden " ref={mojitoRef}>MOJITO</h1>
+        <img src="/images/hero-left-leaf.png" alt="left-leaf" className="left-leaf"/>
+        <img src="/images/hero-right-leaf.png" alt="right-leaf" className="right-leaf"/>
 
-          <div className="view-cocktails">
-            <p className="subtitle">
-              Every cocktil on our menu is a blend of premium ingredients, cerative flair, and timeless recipes -
-              designed to delight your senses.
-            </p>
-            <a href="#coctails">View Cocktails</a>
+        <div className="body">
+          <div className="content">
+            <div className="space-y-5 hidden md:block">
+              <p>Cool. Crisp. Classic.</p>
+              <p className="subtitle">
+                Sip the Spirit <br/> of Summer
+              </p>
+            </div>
+
+            <div className="view-cocktails">
+              <p className="subtitle">
+                Every cocktil on our menu is a blend of premium ingredients, cerative flair, and timeless recipes -
+                designed to delight your senses.
+              </p>
+              <a href="#coctails">View Cocktails</a>
+            </div>
           </div>
         </div>
+      </section>
+
+      <div className="video absolute inset-0">
+        <video ref={videoRef} src="/src/output.mp4" muted playsInline preload='auto'></video>
       </div>
 
-
-    </section>
+    </>
   );
 };
 
