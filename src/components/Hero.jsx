360467e031cf0240dd4a7a645f0c317b355a1f7d
Marina2022 <marusiiiia@yandex.ru>
2025-08-08 13:21:53 +0300

    ff

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -108,9 +108,8 @@ const Hero = () => {
       </section>
 
       <div className="video absolute inset-0">
-        <video ref={videoRef} src="/src/output.mp4" muted playsInline preload='auto'></video>
+        <video ref={videoRef} src="/videos/output.mp4" muted playsInline preload='auto'></video>
       </div>
-
     </>
   );
 };
