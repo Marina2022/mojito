e331d3b883f82542b2b6385f874f9ba7031aaf48
Marina2022 <marusiiiia@yandex.ru>
2025-08-08 13:28:14 +0300

    fix-3

--- src/components/Menu.jsx
+++ src/components/Menu.jsx
@@ -50,7 +50,7 @@ const Menu = () => {
 
   return (
     <section id="menu">
-      <img src="/images/slider-left-leaf.png" alt="left-leaf" id="m-left-leaf"/>
+      {/*<img src="/images/slider-left-leaf.png" alt="left-leaf" id="m-left-leaf"/>*/}
       <img src="/images/slider-right-leaf.png" alt="right-leaf" id="m-right-leaf"/>
 
       <h2 id="menu-heading" className="sr-only">
